c1f75d4e931c3dc0635f763543b8337c4dd79e6c
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-08 15:27:28 -0300

    update time js

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
 import Camera from "./Camera";
 import Renderer from "./Renderer";
 
@@ -11,6 +12,7 @@ export default class Experience {
     Experience.instance = this;
     this.canvas = canvas;
     this.scene = new THREE.Scene();
+    this.time = new Time();
     this.sizes = new Sizes();
     this.camera = new Camera();
     this.renderer = new Renderer();
