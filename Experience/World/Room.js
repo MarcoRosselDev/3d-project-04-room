9d13bf9a71be75c7659a7491b9b141e707b7b9ec
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-08 15:45:13 -0300

    world folder

---
+++ Experience/World/Room.js
@@ -0,0 +1,12 @@
+import Experience from "./Experience";
+import * as THREE from "three";
+
+export default class Room {
+  constructor() {
+    this.experience = new Experience();
+  }
+
+  resize() {}
+
+  update() {}
+}
---
+++ Experience/World/World.js
@@ -0,0 +1,16 @@
+import Experience from "./Experience";
+import * as THREE from "three";
+
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+    this.camera = this.experience.camera;
+  }
+
+  resize() {}
+
+  update() {}
+}
