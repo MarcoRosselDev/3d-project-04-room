814bfec84352b51db9ad05fd458e886211d8c8f4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-09 14:13:28 -0300

    assets

--- Experience/Utils/Resources.js
+++ Experience/Utils/Resources.js
@@ -2,9 +2,12 @@ import { EventEmitter } from "events";
 import Experience from "../Experience.js";
 
 export default class Resources extends EventEmitter {
-  constructor() {
+  constructor(assets) {
     super();
     this.experience = new Experience();
     this.renderer = this.experience.renderer;
+
+    this.assets = assets;
+    console.log(this.assets);
   }
 }
