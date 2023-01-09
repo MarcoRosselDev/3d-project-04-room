706bac366c04200426d81ac66bac18cc5d506012
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-09 15:41:45 -0300

    adding perspective camera color fixed

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -12,7 +12,7 @@ export default class Environment {
   }
 
   setSunlight() {
-    this.sunLight = new THREE.DirectionalLight("0xffffff", 3);
+    this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
     this.sunLight.castShadow = true;
     this.sunLight.shadow.camera.far = 20;
     this.sunLight.shadow.mapSize.set(1024, 1024);
