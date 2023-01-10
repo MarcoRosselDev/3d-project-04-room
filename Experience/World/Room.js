4e2ddcabe117112a3a98c1deda71a43e7b509a11
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-10 16:15:18 -0300

    movement of fish as successful

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -5,12 +5,13 @@ export default class Room {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
-
     this.resources = this.experience.resources;
+    this.time = this.experience.time;
     this.room = this.resources.items.room;
     this.actualRoom = this.room.scene;
 
     this.setModel();
+    this.setAnimation();
   }
 
   setModel() {
@@ -45,7 +46,15 @@ export default class Room {
     this.actualRoom.scale.set(0.11, 0.11, 0.11);
   }
 
+  setAnimation() {
+    this.mixer = new THREE.AnimationMixer(this.actualRoom);
+    this.swim = this.mixer.clipAction(this.room.animations[0]);
+    this.swim.play();
+  }
+
   resize() {}
 
-  update() {}
+  update() {
+    this.mixer.update(this.time.delta * 0.0009);
+  }
 }
