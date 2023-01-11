52edf59b401623d9ad14051b1004b4308cda7ba4
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-11 18:20:31 -0300

    aquarium fixed 2

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -13,16 +13,20 @@ export default class Room {
     this.dummyCurve = new THREE.Vector3(0, 0, 0);
 
     this.setPath();
+    this.onWheel();
   }
 
   setPath() {
-    this.curve = new THREE.CatmullRomCurve3([
-      new THREE.Vector3(-10, 0, 10),
-      new THREE.Vector3(-5, 5, 5),
-      new THREE.Vector3(0, 0, 0),
-      new THREE.Vector3(5, -5, 5),
-      new THREE.Vector3(10, 0, 10),
-    ]);
+    this.curve = new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(-10, 0, 10),
+        new THREE.Vector3(-5, 5, 5),
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(5, -5, 5),
+        new THREE.Vector3(10, 0, 10),
+      ],
+      true
+    );
 
     const points = this.curve.getPoints(50);
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
@@ -33,11 +37,18 @@ export default class Room {
     this.scene.add(curveObject);
   }
 
+  onWheel() {
+    console.log("hola");
+  }
+
   resize() {}
 
   update() {
     this.curve.getPointAt(this.progress % 1, this.dummyCurve);
-    this.progress += 0.01;
+    this.progress -= 0.01;
+    if (this.progress < 0) {
+      this.progress = 1;
+    }
     this.camera.orthographicCamera.position.copy(this.dummyCurve);
   }
 }
