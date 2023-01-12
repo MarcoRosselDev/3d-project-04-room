59b7d048da3ded7d2adbf102fa05152b2e24a67b
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-12 05:04:54 -0300

    set floor

---
+++ Experience/World/Floor.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+import Experience from "../Experience";
+
+export default class Floor {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+
+    this.setFloor();
+  }
+
+  setFloor() {}
+
+  resize() {}
+
+  update() {}
+}
